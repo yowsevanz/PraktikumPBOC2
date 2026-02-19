a1ad55666c221ab68ffb4d4933927c701a46579a
yasmina syahidah <yasminasyahidah@gmail.com>
2026-02-19 16:47:57 +0700

    Pertemuan1

--- Pertemuan1/src/MTitik.java
+++ Pertemuan1/src/MTitik.java
@@ -1,8 +1,13 @@
+
+
 public class MTitik {
     public static void main(String[] args) {
         Titik T1 = new Titik();
         T1.setAbsis(3);
         T1.setOrdinat(4);
         T1.printTitik();
+        T1.geser(3, 4);
+        T1.printTitik();
+
     }
 }
