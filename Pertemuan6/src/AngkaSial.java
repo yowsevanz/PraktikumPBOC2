6c625cb299e8332962b7a0c1360829dd21c83192
yasmina syahidah <yasminasyahidah@gmail.com>
2026-03-26 22:18:10 +0700

    Asersi dan Eksepsi

---
+++ Pertemuan6/src/AngkaSial.java
@@ -0,0 +1,20 @@
+public class AngkaSial {
+    public void cobaAngka(int angka) throws AngkaSialException{
+        if (angka==13) {
+            throw new AngkaSialException();
+        }
+        System.out.println(angka+" bukan angka sial");
+    }
+    public static void main(String[] args) {
+        AngkaSial as = new AngkaSial();
+        try {
+            as.cobaAngka(10);
+            as.cobaAngka(13);
+            as.cobaAngka(12);
+        } catch (AngkaSialException ase) {
+            // 
+            System.out.println(ase.getMessage());
+            System.out.println("hati-hati masukkan angka!!!");
+        }
+    }
+}
---
+++ Pertemuan6/src/AngkaSialException.java
@@ -0,0 +1,5 @@
+public class AngkaSialException extends Exception {
+    public AngkaSialException(){
+        super("jangan memasukkan angka 13 karena angka sial !!!");
+    }
+}
